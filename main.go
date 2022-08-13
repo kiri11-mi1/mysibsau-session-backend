bb49bec5bd82efaa6d46792fe2a8728b59e23991
Kirill Mil <kirill.mil.0310@mail.ru>
2022-08-13 13:32:20 +0700

    create simple web server

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+type Group struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func session(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if recover() != nil {
+			http.Error(w, "Ошибка на стороне сервера", http.StatusInternalServerError)
+		}
+	}()
+	g := Group{Id: 4, Name: "РС18-01"}
+	err := json.NewEncoder(w).Encode(&g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	var router = mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/session", session).Methods("GET")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", router))
 }
