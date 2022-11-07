532b187a499baf566ab23026ef3e835e3f8c7983
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-08 01:03:25 +0700

    call get session

--- src/main.go
+++ src/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"github.com/caarlos0/env/v6"
+	"github.com/skilld-labs/go-odoo"
 	"log"
+	"mysibsau-session-backend/pallada"
 	"net/http"
 )
 
+var creds credentials
+
 type Group struct {
-	Id   int    `json:"id"`
+	Id   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -25,8 +30,29 @@ func session(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func foo() {
+	// Авторизация под аккаунтом администратора
+	if err := env.Parse(&creds); err != nil {
+		log.Fatalln(err)
+	}
+	client, err := odoo.NewClient(&odoo.ClientConfig{
+		Admin:    creds.Admin,
+		Password: creds.Password,
+		Database: creds.Database,
+		URL:      fmt.Sprintf("https://%s.pallada.sibsau.ru", creds.Database),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var id int64 = 11806
+	api := pallada.OdooAPI{Client: client}
+	fmt.Println(api.GetSessionByGroupID(id))
+
+}
+
 func main() {
-	var router = mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/session", session).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	foo()
+	//var router = mux.NewRouter().StrictSlash(true)
+	//router.HandleFunc("/session", session).Methods("GET")
+	//log.Fatal(http.ListenAndServe(":8080", router))
 }
