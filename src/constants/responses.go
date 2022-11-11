2ae5b99b7e31d115bf979de0c0e83d134bb5f285
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-11 21:42:14 +0700

    refactor: replace responses

---
+++ src/constants/responses.go
@@ -0,0 +1,9 @@
+package constants
+
+var GROUP_NOT_FOUND = map[string]string{
+	"message": "Не удалось найти группу. Проверьте, верно ли вы написали название.",
+}
+
+var SESSION_NOT_FOUND = map[string]string{
+	"message": "Не удалось найти ваше расписание, возможно оно появится позже.",
+}
--- src/pallada/responses.go
+++
@@ -1,9 +0,0 @@
-package pallada
-
-var GROUP_NOT_FOUND = map[string]string{
-	"message": "Не удалось найти группу",
-}
-
-var TIMETABLE_NOT_FOUND = map[string]string{
-	"message": "Не удалось найти расписание",
-}
