f51b3dbaa322ba1330e99acb40459d68b2b209f8
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-11 20:13:47 +0700

    add responses from server

---
+++ src/pallada/responses.go
@@ -0,0 +1,9 @@
+package pallada
+
+var GROUP_NOT_FOUND = map[string]string{
+	"message": "Не удалось найти группу",
+}
+
+var TIMETABLE_NOT_FOUND = map[string]string{
+	"message": "Не удалось найти расписание",
+}
